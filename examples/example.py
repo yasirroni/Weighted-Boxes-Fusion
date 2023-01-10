087933b445b23fbc69714d5dd1daf7cb57bf49d8
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2023-01-10 14:08:27 +0700

    explore wbf

--- examples/example.py
+++ examples/example.py
@@ -114,51 +114,20 @@ def make_image_box(boxes, names=None, name_to_id=None, scores=None, img_size=(10
 # ## Multi model
 
 # %%
-boxes_list = [
-    [
-        [0.00, 0.51, 0.81, 0.91],
-        [0.10, 0.31, 0.71, 0.61],
-        [0.01, 0.32, 0.83, 0.93],
-        [0.02, 0.53, 0.11, 0.94],
-        [0.03, 0.24, 0.12, 0.35],
-    ],
-    [
-        [0.04, 0.56, 0.84, 0.92],
-        [0.12, 0.33, 0.72, 0.64],
-        [0.38, 0.66, 0.79, 0.95],
-        [0.08, 0.49, 0.21, 0.89],
-    ],
-]
-scores_list = [
-    [
-        0.9,
-        0.8,
-        0.2,
-        0.4,
-        0.7,
-    ],
-    [
-        0.5,
-        0.8,
-        0.7,
-        0.3,
-    ]
-]
-labels_list = [
-    [
-        0,
-        1,
-        0,
-        1,
-        1,
-    ],
-    [
-        1,
-        1,
-        1,
-        0,
-    ]
-]
+boxes_list = [[
+    [0.00, 0.51, 0.81, 0.91],
+    [0.10, 0.31, 0.71, 0.61],
+    [0.01, 0.32, 0.83, 0.93],
+    [0.02, 0.53, 0.11, 0.94],
+    [0.03, 0.24, 0.12, 0.35],
+],[
+    [0.04, 0.56, 0.84, 0.92],
+    [0.12, 0.33, 0.72, 0.64],
+    [0.38, 0.66, 0.79, 0.95],
+    [0.08, 0.49, 0.21, 0.89],
+]]
+scores_list = [[0.9, 0.8, 0.2, 0.4, 0.7], [0.5, 0.8, 0.7, 0.3]]
+labels_list = [[0, 1, 0, 1, 1], [1, 1, 1, 0]]
 weights = [2, 1]
 
 # %% [markdown]
@@ -170,10 +139,18 @@ scores_list_flat = [i_ for i in scores_list for i_ in i]
 labels_list_flat = [i_ for i in labels_list for i_ in i]
 
 # %% [markdown]
+# # Params
+
+# %%
+iou_thr = 0.5
+skip_box_thr = 0.0001
+sigma = 0.1
+
+# %% [markdown]
 # # Multi model
 
 # %%
-im_ori = make_image_box(boxes_list_flat, names=labels_list_flat, scores=scores_list_flat)
+im_ori = make_image_box(boxes_list_flat, names=range(len(boxes_list_flat)), scores=scores_list_flat)
 
 fig = plt.figure(figsize=(10,10))
 ax = fig.add_subplot(1, 1, 1)
@@ -183,24 +160,15 @@ plt.show()
 
 # %%
 # WBF
-iou_thr = 0.55
 boxes, scores, labels = weighted_boxes_fusion(boxes_list, scores_list, labels_list, weights=weights, iou_thr=iou_thr, skip_box_thr=0.0)
 im_wbf = make_image_box(boxes, names=labels.astype(np.int32), scores=scores)
 
-# soft-nms
-iou_thr = 0.5
-method = 2 # gaussian soft-nms
-sigma = 0.5
-thresh=0.0
-boxes, scores, labels = nms_method(boxes_list, scores_list, labels_list, method=method, weights=weights, iou_thr=iou_thr, sigma=sigma, thresh=thresh)
+# soft-nms (gaussian)
+boxes, scores, labels = soft_nms(boxes_list, scores_list, labels_list, method=2, weights=weights, iou_thr=iou_thr, sigma=sigma, thresh=skip_box_thr)
 im_soft_nms = make_image_box(boxes, names=labels.astype(np.int32), scores=scores)
 
 # nms
-iou_thr = 0.3
-method = 3 # standard nms
-sigma = 0.5
-thresh = 0.001
-boxes, scores, labels = nms_method(boxes_list, scores_list, labels_list, method=method, weights=weights, iou_thr=iou_thr, sigma=sigma, thresh=thresh)
+boxes, scores, labels = nms(boxes_list, scores_list, labels_list, weights=weights, iou_thr=iou_thr)
 im_nms = make_image_box(boxes, names=labels.astype(np.int32), scores=scores)
 
 # %%
@@ -264,24 +232,15 @@ plt.show()
 
 # %%
 # WBF
-iou_thr = 0.55
-boxes, scores, labels = weighted_boxes_fusion([boxes_list_flat], [scores_list_flat], [labels_list_flat], weights=None, iou_thr=iou_thr, skip_box_thr=0.0)
+boxes, scores, labels = weighted_boxes_fusion([boxes_list_flat], [scores_list_flat], [labels_list_flat], weights=None, iou_thr=iou_thr, skip_box_thr=skip_box_thr)
 im_wbf = make_image_box(boxes, names=labels.astype(np.int32), scores=scores)
 
-# soft-nms
-iou_thr = 0.5
-method = 2 # gaussian soft-nms
-sigma = 0.5
-thresh=0.0
-boxes, scores, labels = nms_method([boxes_list_flat], [scores_list_flat], [labels_list_flat], method=method, weights=None, iou_thr=iou_thr, sigma=sigma, thresh=thresh)
+# soft-nms (gaussian)
+boxes, scores, labels = soft_nms([boxes_list_flat], [scores_list_flat], [labels_list_flat], method=2, weights=None, iou_thr=iou_thr, sigma=sigma, thresh=skip_box_thr)
 im_soft_nms = make_image_box(boxes, names=labels.astype(np.int32), scores=scores)
 
 # nms
-iou_thr = 0.3
-method = 3 # standard nms
-sigma = 0.5
-thresh = 0.001
-boxes, scores, labels = nms_method([boxes_list_flat], [scores_list_flat], [labels_list_flat], method=method, weights=None, iou_thr=iou_thr, sigma=sigma, thresh=thresh)
+boxes, scores, labels = nms([boxes_list_flat], [scores_list_flat], [labels_list_flat], weights=None, iou_thr=iou_thr)
 im_nms = make_image_box(boxes, names=labels.astype(np.int32), scores=scores)
 
 # %%
